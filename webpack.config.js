748ff2b462139fbfd17ac89789747090d1ffc9f2
Andrius Zidonis <zidandrius@gmail.com>
2020-07-05 22:21:46 +0300

    update dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
+const path = require('path');
+
 module.exports = {
     mode: process.env.NODE_ENV === 'PROD' ? 'production' : 'development',
-    entry: {
-        app: './index.js',
+    devServer: {
+        contentBase: path.join(__dirname, 'www'),
+        compress: true,
+        port: 9000
     },
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        filename: './bundle.js',
+        path: path.resolve(__dirname, 'www/builds'),
+        filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(css)|(.scss)$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    devtool: 'source-map'
 };
