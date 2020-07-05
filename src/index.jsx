bf47ea4b43dd6fb1325cc9d41a846fd986a9a68a
Andrius Zidonis <zidandrius@gmail.com>
2020-07-05 23:45:36 +0300

    update dev flow

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,5 @@
-alert("entry file");
+import 'react-hot-loader';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
     mode: process.env.NODE_ENV === 'PROD' ? 'production' : 'development',
     devServer: {
-        contentBase: path.join(__dirname, 'www'),
-        compress: true,
-        port: 9000
+        contentBase: path.join(__dirname, "www/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true,
+        disableHostCheck: true,
     },
     entry: path.resolve(__dirname, 'src/index.jsx'),
     output: {
-        path: path.resolve(__dirname, 'www/builds'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
     },
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.(js|jsx)$/,
                 exclude: path.resolve(__dirname, 'node_modules'),
                 include: path.resolve(__dirname, 'src'),
                 use: {
@@ -26,18 +30,27 @@ module.exports = {
                 }
             },
             {
-                test: /\.(css)|(.scss)$/,
-                include: path.resolve(__dirname, 'src'),
+                test: /\.css$/,
                 use: [
-                    // Creates `style` nodes from JS strings
                     'style-loader',
-                    // Translates CSS into CommonJS
                     'css-loader',
-                    // Compiles Sass to CSS
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
                     'sass-loader',
                 ],
             }
         ]
     },
-    devtool: 'source-map'
+    resolve: {
+        alias: {
+          'react-dom': process.env.NODE_ENV === 'PROD' ? 'react-dom' : '@hot-loader/react-dom'
+        }
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 };
